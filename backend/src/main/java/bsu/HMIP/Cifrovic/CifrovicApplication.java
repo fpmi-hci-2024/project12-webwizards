bb1e9dfdf2ab4498f9311c86b93922ce20002e79
ilyaayana <ilyaayana92@gmail.com>
2024-11-18 21:38:06 +0300

    initial commit

---
+++ backend/src/main/java/bsu/HMIP/Cifrovic/CifrovicApplication.java
@@ -0,0 +1,13 @@
+package bsu.HMIP.Cifrovic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifrovicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CifrovicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bsu/HMIP/Cifrovic/CifrovicApplicationTests.java
@@ -0,0 +1,13 @@
+package bsu.HMIP.Cifrovic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifrovicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
